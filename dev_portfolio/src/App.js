4e2fa9e391fc6449c75c65ad219876d44f81cf09
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2020-12-28 22:43:10 +0530

    setup initial project

--- dev_portfolio/src/App.js
+++ dev_portfolio/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Uff! Finally</h1>
+      <div className="container">
+        <div className="row">
+            <div className="col-lg-3">
+            <div className="app-sidebar">
+                <Sidebar />
+            </div>
+            </div>
+            <div className="col-lg-9 app-main-content">
+                Main
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
